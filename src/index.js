c3ec16a8cd7f2c5f3c5cc8f537279cf113df984e
Mithi <mithi.sevilla@gmail.com>
2021-01-01 16:39:12 +0800

    🔥 Remove service worker

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React, { Suspense } from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
-import * as serviceWorker from "./serviceWorker"
 
-const App = React.lazy(() =>
-    import(/* webpackChunkName: "APP", webpackPreload: true */ "./App")
-)
+const App = React.lazy(() => import("./App"))
 
 ReactDOM.render(
     <React.StrictMode>
@@ -15,8 +12,3 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById("root")
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register()
