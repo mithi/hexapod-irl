c127c583a53a98d9d0311f0b2bfd518d754d4f41
Mithi <mithi.sevilla@gmail.com>
2021-01-03 06:34:50 +0800

    🔥 Remove CoC and Contributing

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -11,16 +11,6 @@ class LandingPage extends React.Component {
         <>
             <div id="landing">
                 <RandomRobotGif />
-                <h1>Mithi's Bare Minimum Hexapod Robot Simulator</h1>
-                <p>
-                    This page might feel slow at first, because it is also eagerly loading
-                    and mounting the 3d plot behind the scenes! You can actually already
-                    navigate to any of the links below right now (which I recommend), even
-                    if this page hasn't fully loaded yet.
-                </p>
-                <p>
-                    This app works offline! Enjoy your stay and share with your friends!
-                </p>
             </div>
         </>
     )
